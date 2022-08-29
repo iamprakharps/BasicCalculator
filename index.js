46679657f9271e3642397b2d01e40d7b72275067
Prakhar <94009078+iamprakharps@users.noreply.github.com>
2022-08-29 13:10:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var display = document.getElementById('display');
+var buttons = document.getElementsByClassName('buttons');
+
+var operand1 =0;
+var operand2 =null;
+var operator =null;
+
+
+for(var i = 0; i< buttons.length ; i++){
+    buttons[i].addEventListener("click", function (){
+        var value = this.getAttribute('data-value');
+
+        if(value == 'reset') {
+            display.innerText = null;
+        }
+
+        else if(value == '+' || value == '-' || value == "*" || value == "/" || value == "%"){
+        
+                operator = value;
+                operand1 = parseFloat(display.innerText);
+                console.log("opera 1 = ",operand1);
+                display.innerText = null;
+        }
+
+        else if(value == '='){            
+            if(operator != null){
+                operand2 = parseFloat(display.innerText);
+                display.innerText = eval(operand1+" "+operator+" "+operand2);
+            }
+            else{
+                display.innerText = "Error";
+            }
+        }
+
+        else if(value == 'sign'){
+            display.innerText = eval(parseFloat(display.innerText) * (-1) );
+        }
+
+        else if(value == '.') {
+            if(display.innerText.length && !display.innerText.includes('.') ) {
+                display.innerText += value;
+            }
+        }
+        else{
+            display.innerText += value;
+        }
+
+
+    });
+}
